f6d0b47fa5656a5f63bae49cffe00d33fc01011c
Bharat Kumar P <bharatkumarp28@gmail.com>
2021-12-11 13:17:19 +0530

    Basic Code for On;ine Test

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'data/Java.json', name: 'Java Quiz' },
+      { id: 'data/JSON.json', name: 'JSON Quiz' },
+      { id: 'data/JavaScript.json', name: 'JavaScript Quiz' },
+    ];
+  }
+
+}
